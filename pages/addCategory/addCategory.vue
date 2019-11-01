81c85a8481ddc4206ffc5e2d16ad870c109d050e
jly-git <806784058@qq.com>
2019-11-01 17:14:38 +0800

    后台管理

---
+++ pages/addCategory/addCategory.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<view>
+			<input v-model="name" placeholder="输入类别" type="text">
+		</view>
+		<button @tap="addCategory">确定</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				name:''
+			}
+		},
+		methods: {
+			addCategory(){
+				uni.request({
+						url: 'http://localhost:3000/api/category',
+						method: 'POST',
+						data: {
+							name: this.name,
+						},
+						success: res => {
+							console.log('添加分类',res);
+						}
+					});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="content">
+		<button @tap="selectProduct">按分页查询商品</button>
+		<button @tap="addProduct">添加商品</button>
+		
+		<button @tap="selectCategory">查询所有分类</button>
+		<button @tap="addCategory">添加分类</button>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			
+		};
+	},
+	onLoad() {
+		
+	},
+	methods: {
+		selectCategory(){
+			uni.navigateTo({
+				url:'../selectCategory/selectCategory'
+			})
+		},
+		addCategory(){
+			uni.navigateTo({
+				url:'../addCategory/addCategory'
+			})
+		},
+		addProduct(){
+			uni.navigateTo({
+				url:'../addProduct/addProduct'
+			})
+		},
+		selectProduct(){
+			uni.navigateTo({
+				url:'../selectProducts/selectProducts'
+			})
+		},
+	}
+};
+</script>
+
+<style></style>
